b4a83b76c8d0d2875b6eae071fd23365055af3e6
nanfengpo <lsxxxxxx@126.com>
2017-09-14 15:55:44 +0800

    add comment

---
+++ blogproject/comment/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class Comment(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=255)
+    url = models.URLField(blank=True)
+    text = models.TextField()
+    crrad_time = models.DateTimeField(auto_now_add=True)#时间自动生成
+
+    post = models.ForeignKey('blog.Post')
+
+    def __str__(self):
+        return self.text[:20]
