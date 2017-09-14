c9a623d1176abb2d74081b2609dd74bc1e852b4b
nanfengpo <lsxxxxxx@126.com>
2017-09-14 20:52:03 +0800

    add comment

--- blogproject/blog/models.py
+++ blogproject/blog/models.py
@@ -18,6 +18,7 @@ class Post(models.Model):
     body = models.TextField()
     created_time = models.DateTimeField()
     modified_time = models.DateTimeField()
+    # 摘要
     excerpt = models.CharField(max_length=256,blank=True)
 
     #关系
@@ -25,8 +26,17 @@ class Post(models.Model):
     tage = models.ManyToManyField(Tag,blank=True)
 
     author = models.ForeignKey(User)
-
+    # 阅读量
+    views = models.PositiveIntegerField(default=0)
+    def increase_views(self):
+        self.views += 1
+        #update_fields 为只定义一个需要更新的函数
+        self.save(update_fields=['views'])
     #看不到的属性comment_set
+
+    # ordering 属性用来指定文章排序方式，['-created_time'] 指定了依据哪个属性的值进行排序
+    class Meta:
+        ordering = ['-created_time','-modified_time']
 
     #获取绝对路径
     def get_absolute_url(self):
