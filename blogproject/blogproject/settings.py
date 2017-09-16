95f057df0722e00555da448c182b04bb94a3deac
nanfengpo <lsxxxxxx@126.com>
2017-09-16 16:46:44 +0800

    打完收工！！！

--- blogproject/blogproject/settings.py
+++ blogproject/blogproject/settings.py
@@ -145,7 +145,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.163.com'
 #端口号
 EMAIL_PORT = 25
-# 用户名好=和密码
-EMAIL_HOST_USER = 's489397771@163.com'
-EMAIL_HOST_PASSWORD = 'ZITONG78187'
-DEFAULT_FROM_EMAIL = 's489397771@163.com'
+# 用户名和密码
+EMAIL_HOST_USER = '*******@163.com'
+EMAIL_HOST_PASSWORD = '***********'#授权码而非密码#
+DEFAULT_FROM_EMAIL = '********@163.com'
